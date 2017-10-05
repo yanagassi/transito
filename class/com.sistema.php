0a8620de69a941c0ef73a2008a417c7c5b1d58d9
Yan Esteves <yanagassi@outlook.com>
2017-10-05 18:15:16 -0300

    Add files via upload

--- class/com.sistema.php
+++ class/com.sistema.php
@@ -79,9 +79,11 @@ error_reporting(E_ALL^E_NOTICE);
 
 		function token($user)
 		{
+			$ip = $_SERVER['SERVER_ADDR'];
 			$token = md5(uniqid(time()));
-			$query = $this->con->prepare("UPDATE usuarios SET token=:token WHERE usuario=:user");
+			$query = $this->con->prepare("UPDATE usuarios SET token=:token, ip=:ip WHERE usuario=:user");
 			$query->bindParam(':token', $token, PDO::PARAM_STR);
+			$query->bindParam(':ip',$ip, PDO::PARAM_STR);
 			$query->bindParam(':user', $user, PDO::PARAM_STR);
 			$query->execute();
 			return $token;
--- view/inicial.php
+++ view/inicial.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="css/inicial.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script type="text/javascript">
 		jQuery(document).ready(function(){
 			jQuery('#enviarM').submit(function(){
